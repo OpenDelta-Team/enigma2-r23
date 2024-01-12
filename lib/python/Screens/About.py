a45073347a264bd77dc552929cb09b8800a34bac
Open Delta Team <86120617+OpenDelta-Team@users.noreply.github.com>
2024-01-12 22:45:09 +0100

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -36,10 +36,10 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
+		AboutText += _("OE Version: Hardknott (3.3.6)") + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
-		# AboutText += _("OE Version: ") + about.getOEVersionString() + "\n"
-		AboutText += _("Build Image: OpenDelta-Team") + "\n"
 		AboutText += _("Last update: 2024-01-06")  + "\n"
+		AboutText += _("Build Image: OpenDelta-Team") + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
